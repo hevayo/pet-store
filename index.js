1e91a69f857f40c005fcbc7afb0e1477aba82b18
Joseph Fonseka <hevayo@gmail.com>
2024-01-23 10:27:15 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const app = express();
+app.use(express.json());
+
+pets = [
+    { id: 1, name: 'Rex', type: 'Dog', age: 5 },
+    { id: 2, name: 'Whiskers', type: 'Cat', age: 3 },
+    { id: 3, name: 'Bubbles', type: 'Fish', age: 1 }
+];
+
+
+app.get('/pets', (req, res) => {
+    res.json(pets);
+});
+
+app.post('/pets', (req, res) => {
+    const pet = req.body;
+    pets.push(pet);
+    res.status(201).send();
+});
+
+app.put('/pets/:id', (req, res) => {
+    const id = req.params.id;
+    const pet = req.body;
+    pets[id] = pet;
+    res.status(200).send();
+});
+
+app.delete('/pets/:id', (req, res) => {
+    const id = req.params.id;
+    pets.splice(id, 1);
+    res.status(200).send();
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
